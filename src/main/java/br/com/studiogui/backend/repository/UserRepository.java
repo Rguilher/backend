1dde29a195353244ba13f4a5180835b3090285d5
Renan Henrique <renan.henri90@icloud.com>
2026-01-02 19:39:21 -0300

    chore(pom.xml): configure connection to db with postgresql

--- src/main/java/br/com/studiogui/backend/repository/UserRepository.java
+++ src/main/java/br/com/studiogui/backend/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.studiogui.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+
 }
